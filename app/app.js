4d4218cf9eb6ae1bad22c5a90f0d124c07ed75cf
Aiwwa <as.savinis@gmail.com>
2021-07-20 21:50:03 +0300

    first commit

---
+++ app/app.js
@@ -0,0 +1,11 @@
+const menuBtn = document.getElementById('trigger');
+const itemsContainer = document.getElementById('itemsContainer');
+
+menuBtn.addEventListener('click', () => {
+  itemsContainer.classList.toggle('d-none')
+});
+
+
+
+
+
